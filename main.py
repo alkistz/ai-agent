b217371f9396bbcd042a765be1d73f98625613e2
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2025-09-04 10:42:32 +0200

    Refactor argument handling and response processing in main function

--- main.py
+++ main.py
@@ -11,21 +11,27 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    if len(sys.argv) < 2:
+    cli_arguements = sys.argv
+    if len(cli_arguements) < 2:
         print("No prompt was provided")
         sys.exit(1)
 
-    user_prompt = sys.argv[1]
+    is_verbose = "--verbose" in cli_arguements
 
+    user_prompt = cli_arguements[1]
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=user_prompt,
-)
+        contents=messages,
+    )
+
+    if is_verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
