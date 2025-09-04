61e5ce3e60b3e42b6510a320fad21bb14c41c191
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2025-09-04 16:36:59 +0200

    Fix variable name typos and clean up debug print statements in get_files_info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,24 +3,28 @@ import os
 
 def get_files_info(working_directory, directory="."):
     root_directory = "/home/alkistz/dev/bootdev/ai-agent"
-    absolut_working_directory = os.path.join(root_directory, working_directory)
+    absolute_working_directory = os.path.join(root_directory, working_directory)
     relative_path = os.path.join(working_directory, directory)
-    absolut_path = os.path.abspath(relative_path)
+    absolute_path = os.path.abspath(relative_path)
 
-    if not absolut_path.startswith(absolut_working_directory):
+    if not absolute_path.startswith(absolute_working_directory):
         print("Outside of boundries")
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(absolut_path):
+    if not os.path.isdir(absolute_path):
         print("Not a directory")
         return f'Error: "{directory}" is not a directory'
 
-    dir_contents = os.listdir(absolut_path)
-    print(os.path.isdir(absolut_path))
+    dir_contents = os.listdir(absolute_path)
 
-    print(relative_path)
-    print(absolut_path)
-    print(dir_contents)
+    print(f"Result for '{directory}' directory")
+
+    for file in dir_contents:
+        file_path = os.path.join(absolute_path, file)
+        is_dir = os.path.isdir(file_path)
+        file_size = os.path.getsize(file_path)
+        print(f"- {file}: file_size={file_size} bytes, is_dir={is_dir}")
 
 
+# get_files_info("calculator", "../")
 get_files_info("calculator", ".")
